4ad6e60461c9346c2b2026ffb4646826156ba116
Laura <you@example.com>
2020-11-15 14:08:39 +0200

    korjauksia

--- find-the-address/App.js
+++ find-the-address/App.js
@@ -1,52 +1,78 @@
-import React, {useState} from 'react';
-import { View, TextInput, Button } from 'react-native';
+import React from 'react';
+import { StyleSheet, View, TextInput, Button } from 'react-native';
 import MapView, { Marker } from 'react-native-maps';
 
 export default function App() {
 
-  const [address, setAddress] = useState('');
-  const [coordinates, setCoordinates] = useState({latitude:60.201373, longitude: 24.934041});
-  const [region, setRegion] = useState({  latitude: 60.200692,
-                                          longitude: 24.934302,
-                                          latitudeDelta: 0.0322,
-                                          longitudeDelta: 0.0221,
-                                        });
+  const [address, setAddress] = React.useState('');
+  const [coordinates, setCoordinates] = React.useState({latitude: 60.201373, longitude: 24.934041});
+  const [region, setRegion] = React.useState({
+                              latitude: 60.200692,
+                              longitude: 24.934302,
+                              latitudeDelta: 0.0322,
+                              longitudeDelta: 0.0221});
 
-  const findAddress = () => {
-    fetch('https://www.mapquestapi.com/geocoding/v1/address?key=Xg7NJSbNK0AM3bRIm6DGbtxKsQBHNWSF&location='+ address)
-    .then((response) => response.json())
-    .then((data) => { 
-      setCoordinates({latitude: data.results.locations.latLng.lat, longitude: data.results.locations.latLng.lng});
-      setRegion({latitude: (coordinates.latitude-0.002), longitude: (coordinates.longitude+0.0006)});
+  const getCoordinates = () => {
+    const url = `http://www.mapquestapi.com/geocoding/v1/address?key=Xg7NJSbNK0AM3bRIm6DGbtxKsQBHNWSF&location=${address}`;
+    fetch(url)
+    .then((response) => response.json()) 
+    .then((data) => {
+      setRegion({...region, latitude: (coordinates.latitude), longitude: (coordinates.longitude)});
+      setCoordinates({latitude: data.results[0].locations[0].latLng.lat, longitude: data.results[0].locations[0].latLng.lng});
+      
     })
+    .catch((error)=>{
+        Alert.alert('Error', error.message);
+    });
   }
 
   return (
-   <View>
-      <MapView
-        style={{ width: 500, height: 630}}
-        initialRegion= {{ region }}
-        region={{ region }}
-      > 
-          
-        <Marker
-            coordinate={{ coordinates }}
-            title={address}
+    <View style={styles.container}>
+      <MapView 
+        style={styles.map}
+        initialRegion={region}
+        region={region}
+      >
+        <Marker 
+          coordinate={coordinates}
+          title={address}
         />
-          
       </MapView>
-      <View>
-      <TextInput 
-        style={{ 
-          width:430, 
-          borderColor: 'gray', 
-          borderWidth: 1
-          }}
-        onChangeText={ address =>  setAddress(address)} value={address} />
-      <Button onPress={ findAddress } title="Etsi"/>
+      <View style={styles.input}>
+        <TextInput 
+          keyboardType="default"
+          onChangeText={(text) => setAddress(text)}
+          value={address}
+        />
+        <Button
+          onPress={getCoordinates}
+          title="Etsi"/>
       </View>
-      
-      </View>
+    </View>
   );
 }
 
+const styles = StyleSheet.create({
+  container: {
+    paddingTop: 5,
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  input:{
+    flex: 1,
+    alignItems: 'center',
+    width: 500,
+    margin: 2,
+    padding: 10,
+    borderWidth: 0.5,
+    borderRadius: 4,
+    backgroundColor: "#fff"
+   },
+   map: {
+    flex: 9,
+    width: '100%',
+    height: '100%'
+   }
+});
